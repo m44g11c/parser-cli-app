bbfa1ebc5ec71430f99ef277a2e2aa6bc1e09833
m44g11c <m44g11c@gmail.com>
2019-12-22 20:02:57 +0300

    import-to-db

--- src/Command/CsvImportCommand.php
+++ src/Command/CsvImportCommand.php
@@ -7,30 +7,62 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use App\Entity\Product;
+use League\Csv\Reader;
+use League\Csv\Statement;
 
 class CsvImportCommand extends Command
 {
-    protected static $defaultName = 'app:csv-import';
+  protected static $defaultName = 'app:csv-import';
 
-    protected function configure()
-    {
-        $this
-            ->setDescription('CSV import')
-            ->addArgument('test', InputArgument::OPTIONAL, 'Test mode')
-        ;
+  protected function configure()
+  {
+    $this
+      ->setDescription('CSV import')
+      ->addArgument('test', InputArgument::OPTIONAL, 'Test mode');
+  }
+
+  protected function execute(InputInterface $input, OutputInterface $output): int
+  {
+    $io = new SymfonyStyle($input, $output);
+    $test = $input->getArgument('test');
+
+    if ($test) {
+      $io->note(sprintf('You passed an argument: %s', $test));
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        $io = new SymfonyStyle($input, $output);
-        $test = $input->getArgument('test');
+    $reader = Reader::createFromPath('%kernel.root_dir%/../data/stock.csv');
+    $reader->setHeaderOffset(0);
+    $records = (new Statement())->process($reader);
 
-        if ($test) {
-            $io->note(sprintf('You passed an argument: %s', $test));
-        }
+    $io->title('Importing...');
+    $io->progressStart(iterator_count($records));
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+    $kernel = $this->getApplication()->getKernel();
+    $container = $kernel->getContainer();
+    $em = $container->get('doctrine')->getManager();
 
-        return 0;
+    foreach ($records as $record) {
+
+      $record['Cost in GBP']  = floatval($record['Cost in GBP']);
+      $record['Stock']  = intval($record['Stock']);
+
+      $product = new Product();
+      $product->setProductCode($record['Product Code']);
+      $product->setProductName($record['Product Name']);
+      $product->setProductDescription($record['Product Description']);
+      $product->setStock($record['Stock']);
+      $product->setCostinGBP($record['Cost in GBP']);
+      $product->setDiscontinued($record['Discontinued']);
+
+      $em->persist($product);
+      $em->flush();
     }
+
+    $io->progressFinish();
+
+    $io->success('Import complete.');
+
+    return 0;
+  }
 }
